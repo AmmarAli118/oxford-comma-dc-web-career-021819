035c2279a20c3e3fa03141a17510079ccd9afb71
Ammar Ali <funnyguy118@gmail.com>
2019-01-25 03:06:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length > 2 
     and_finish = "and #{array.pop}"
     array.push(and_finish)
-  else array.length == 2 
+  else  
     return array.join(" and ")
   end
   array.join(", ")
